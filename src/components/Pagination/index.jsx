4c03391ac74a45b1409ac7ffba89e7ad307344d9
Ibragimov Eldar <1ibragimov.e.i20@gmail.com>
2022-08-12 11:16:20 +0300

    upd

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -4,21 +4,21 @@ import ReactPaginate from "react-paginate";
 import styles from "./Pagination.module.scss";
 
 const Pagination = ({ onChangePage, pageCount }) => {
-    return (
-        <div>
-            {" "}
-            <ReactPaginate
-                className={styles.root}
-                breakLabel="..."
-                nextLabel=">"
-                previousLabel="<"
-                onPageChange={(event) => onChangePage(event.selected + 1)}
-                pageRangeDisplayed={8}
-                pageCount={pageCount}
-                renderOnZeroPageCount={null}
-            />
-        </div>
-    );
+  return (
+    <div>
+      {" "}
+      <ReactPaginate
+        className={styles.root}
+        breakLabel="..."
+        nextLabel=">"
+        previousLabel="<"
+        onPageChange={(event) => onChangePage(event.selected + 1)}
+        pageRangeDisplayed={8}
+        pageCount={pageCount}
+        renderOnZeroPageCount={null}
+      />
+    </div>
+  );
 };
 
 export default Pagination;
