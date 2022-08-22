e2a21c77044a32bf18b01a13b96b093212a1b645
Ibragimov Eldar <90416969+ibragimoov@users.noreply.github.com>
2022-08-22 08:06:06 +0300

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,8 @@ import Search from "./Search";
 export default function Header() {
     const { items, totalPrice } = useSelector((state) => state.cart);
 
+    const totalCount = items.reduce((sum, item) => sum + item.count, 0);
+
     return (
         <div className="header">
             <div className="container">
@@ -56,7 +58,7 @@ export default function Header() {
                                 strokeLinejoin="round"
                             />
                         </svg>
-                        <span>{items.length}</span>
+                        <span>{totalCount}</span>
                     </Link>
                 </div>
             </div>
--- src/components/PizzaBlock/Skeleton.jsx
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import ContentLoader from "react-content-loader";
 
 const Skeleton = (props) => (
-  <ContentLoader
-    className="pizza-block"
-    speed={2}
-    width={280}
-    height={500}
-    viewBox="0 0 280 500"
-    backgroundColor="#f3f3f3"
-    foregroundColor="#ecebeb"
-    {...props}
-  >
-    <circle cx="146" cy="132" r="120" />
-    <rect x="0" y="287" rx="10" ry="10" width="280" height="37" />
-    <rect x="0" y="452" rx="10" ry="10" width="91" height="30" />
-    <rect x="147" y="444" rx="30" ry="30" width="133" height="48" />
-    <rect x="0" y="343" rx="10" ry="10" width="280" height="88" />
-  </ContentLoader>
+    <ContentLoader
+        className="pizza-block"
+        speed={2}
+        width={280}
+        height={500}
+        viewBox="0 0 280 500"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#ecebeb"
+        {...props}
+    >
+        <circle cx="146" cy="132" r="120" />
+        <rect x="0" y="287" rx="10" ry="10" width="280" height="37" />
+        <rect x="0" y="452" rx="10" ry="10" width="91" height="30" />
+        <rect x="147" y="444" rx="30" ry="30" width="133" height="48" />
+        <rect x="0" y="343" rx="10" ry="10" width="280" height="88" />
+    </ContentLoader>
 );
 
 export default Skeleton;
