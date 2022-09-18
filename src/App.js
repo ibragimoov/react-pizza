982f1cfae227732802734a3fb7cfe7568e66dc90
Ibragimov Eldar <90416969+ibragimoov@users.noreply.github.com>
2022-09-18 13:07:34 +0300

    Outlet comp

--- src/App.js
+++ src/App.js
@@ -8,20 +8,18 @@ import FullPizza from "./pages/FullPizza";
 import { Routes, Route } from "react-router-dom";
 import { Cart } from "./pages/Cart";
 import React from "react";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
     return (
-        <div className="wrapper">
-            <Header />
-            <div className="content">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path="/pizza/:id" element={<FullPizza />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </div>
-        </div>
+        <Routes>
+            <Route path="/" element={<MainLayout />}>
+                <Route path="" element={<Home />} />
+                <Route path="cart" element={<Cart />} />
+                <Route path="pizza/:id" element={<FullPizza />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
+        </Routes>
     );
 }
 
